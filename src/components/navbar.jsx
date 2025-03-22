1fad57394b2755c8e7f4dc84334a693996bc1c06
haleychang0530 <haleychang.ee13@nycu.edu.tw>
2025-03-22 23:28:25 +0800

    update navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,19 @@
 // src/components/Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css';
+import './navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <ul>
-        <li><Link to="/home">首頁</Link></li>
-        <li><Link to="/ranking">排行榜</Link></li>
-        <li><Link to="/gamech">中文練習</Link></li>
-        <li><Link to="/gameen">英文練習</Link></li>
+        <li className="logo">
+          <img src="path-to-your-logo.png" alt="Logo" />
+        </li>
+          <li><Link to="/home">首頁🐣</Link></li>
+          <li><Link to="/ranking">排行榜🪽</Link></li>
+          <li><Link to="/gamech">中文練習🐤</Link></li>
+          <li><Link to="/gameen">英文練習🐥</Link></li>
       </ul>
     </nav>
   );
