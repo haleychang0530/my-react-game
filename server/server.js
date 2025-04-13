9c6ca442628af699380803811f40396563859dcb
陳逸安 <kitty532286@gmail.com>
2025-04-13 12:47:36 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ client.connect()
 
 // 允許來自前端之跨域請求
 app.use(cors({
-  origin: 'https://my-react-game-front.onrender.com/#', 
+  origin: 'https://my-react-game-front-uoqw.onrender.com', 
 }));
 // 解析 JSON 請求
 app.use(express.json());
